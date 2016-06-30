601543de1737b9395c899ed13fd484ae0b486126
akixy26 <xiayan26@yahoo.com>
2016-06-30 20:00:48 +0800

    Added interface controllers. Map to be fixed.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,7 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['map.controllers'])
 	.controller("HeaderCtrl", function ($scope, $state) {
     })
 
-    .controller("MapCtrl", function ($scope, $state) {
-    })
 
 	.controller("SceneryListCtrl", function ($scope, $state) {
     	$scope.jumpToScenery = function () {
@@ -11,6 +9,33 @@ angular.module('app.controllers', [])
     	};
 	})
 
-    .controller("FooterCtrl", function ($scope, $state) {
+	.controller("SceneryInfoCtrl", function ($scope, $state) {
+    	$scope.isExpanded = false;
+    	$scope.commentPopup = false;
+    	/* 默认应该设置为后台返回数据，而不是false */
+    	$scope.isWishlist = false;
+    	$scope.isFootprint = false;
+    	$scope.isLike = false;
+
+    	$scope.expandIntro = function () {
+    		$scope.isExpanded = true;
+    	};
+
+    	$scope.showCommentPopup = function () {
+    		$scope.commentPopup = true;
+    	};
+    	$scope.hideCommentPopup = function () {
+    		$scope.commentPopup = false;
+    	};    	
+
+    	$scope.flipWishlist = function () {
+    		$scope.isWishlist = !$scope.isWishlist;
+    	};
+    	$scope.flipFootprint = function () {
+    		$scope.isFootprint = !$scope.isFootprint;
+    	};
+    	$scope.flipLike = function () {
+    		$scope.isLike = !$scope.isLike;
+    	};
     })
 ;
---
+++ www/js/mapControllers.js
@@ -0,0 +1,40 @@
+/* 用于显示、控制地图的控制器 */
+angular.module('map.controllers', [])
+
+.directive('appMap', function() {
+    return {
+        restrict: "E",
+        replace: true,
+        template: "<div id='allMap'></div>",
+        scope: {
+            center: "=",
+            markers: "=",
+            width: "@",
+            height: "@",
+            zoom: "@",
+            zoomControl: "@",
+            scaleControl: "@",
+            address: "@"
+        },
+        link: function (scope, element, attrs) {
+            var map;
+            map = new BMap.Map("allMap");
+            //map.addControl( new BMap.ZoomControl() );
+            var myGeo = new BMap.Geocoder();
+            myGeo.getPoint("静安寺", function(point) {
+                if(!point) {
+                    console.log(point);
+                    map.centerAndZoom( (116.404, 39.915), 16);
+                    map.addOverlay( new BMap.Marker(116.404, 39.915) );
+                }
+                else {
+                    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+                    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+                }
+            }, "上海");
+
+            map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+            map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+        }
+    };
+}) ;
